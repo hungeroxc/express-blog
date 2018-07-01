6718b88dbc334996d86b9c86f2611ad82d210250
oxc <oxc@oxc.com>
2018-07-02 07:25:03 +0800

    up 测试pug

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const path = require('path')
 // 设置模板地址
 app.set('views', path.join(__dirname, 'views'))
 // 设置模板引擎
-app.set('views engine', 'pug')
+app.set('view engine', 'pug')
 
 
 app.get('/', (req, res) => {
-    res.send('ok')
+    res.render('index', {
+        title: '大春'
+    })
 })
 
 app.listen(3000, () => {
