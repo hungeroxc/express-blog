0e9355fb7d4a318ffaba687c43faae789ab8101f
oxc <oxc@oxc.com>
2018-07-03 07:50:50 +0800

    up 完成添加文章的路由

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// 设置post路由读取数据的中间件
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 // 设置模板地址
@@ -26,6 +30,7 @@ db.on('error', err => {
 // 导入数据
 let Article = require('./models/articles')
 
+// 首页读取数据并渲染
 app.get('/', (req, res) => {
     // 读取数据
     Article.find({}, (err, articles) => {
@@ -36,6 +41,25 @@ app.get('/', (req, res) => {
 app.get('/articles/new', (req, res) => {
     res.render('new', {
         title: 'Add Article'
+    })
+})
+
+// 创建文章路由
+app.post('/articles/create',  (req, res) => {
+    let article = new Article()
+    const {title, author, body} = req.body
+    article.title = title
+    article.author = author
+    article.body = body
+
+    article.save(err => {
+        if(err) {
+            console.log(err)
+            return
+        } else {
+            // 此处会跳转首页
+            res.redirect('/')
+        }
     })
 })
 
