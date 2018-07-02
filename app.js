8dd3a9ca84760a4f9ba94be3953df2a4a9ea3d2f
oxc <oxc@oxc.com>
2018-07-03 07:16:42 +0800

    up 使用mongoose链接数据库

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+
+// 链接数据库
+mongoose.connect("mongodb://localhost/express-blog")
+// 获取connection
+const db = mongoose.connection
+// 链接成功监听
+db.once('open', () => {
+    console.log('Connected to Mongodb')
+})
+// 链接出错监听
+db.on('error', err => {
+    console.log(err)
+})
 
 // 设置模板地址
 app.set('views', path.join(__dirname, 'views'))
